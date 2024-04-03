a8c7923687ec974d8fd1c2bd4ca46c832d8858de
davidalantodd <david.todd@multiverse.io>
2024-04-03 16:23:12 -0400

    remove demo code

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,13 +1,8 @@
 import './ControlPanel.css'
 import DatePicker from './DatePicker'
 import SizeSlider from './SizeSlider'
-import Button from '@mui/material/Button';
-import Rating from '@mui/material/Rating';
-import { useState } from 'react'
 
 export default function ControlPanel({ date, setDate, size, setSize }) {
-  const [value, setValue] = useState(2);
-
   return (
     <div className='wrapper'>
       <h2>Control Panel</h2>
@@ -15,14 +10,6 @@ export default function ControlPanel({ date, setDate, size, setSize }) {
       <DatePicker date={date} setDate={setDate} />
       <h3>Size</h3>
       <SizeSlider size={size} setSize={setSize} />
-      <Button variant="contained">I'm a button!</Button>
-      <Rating
-        name="simple-controlled"
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
     </div>
   )
 }
