fe41adb681f7e6a6e15814ae604c2418adce37f4
davidalantodd <david.todd@multiverse.io>
2024-04-04 13:38:26 -0400

    add demo code

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,8 +1,18 @@
 import './ControlPanel.css'
 import DatePicker from './DatePicker'
 import SizeSlider from './SizeSlider'
+import Button from '@mui/material/Button';
+import Rating from '@mui/material/Rating';
+import { useState } from 'react';
+import Typography from '@mui/material/Typography';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 export default function ControlPanel({ date, setDate, size, setSize }) {
+  const [value, setValue] = useState(2);
   return (
     <div className='wrapper'>
       <h2>Control Panel</h2>
@@ -10,6 +20,47 @@ export default function ControlPanel({ date, setDate, size, setSize }) {
       <DatePicker date={date} setDate={setDate} />
       <h3>Size</h3>
       <SizeSlider size={size} setSize={setSize} />
+      <Button variant="contained">Contained</Button>
+      <Typography component="legend">Controlled</Typography>
+      <Rating
+        name="simple-controlled"
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      />
+      <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDownwardIcon />}
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ArrowDropDownIcon />}
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
     </div>
   )
 }
